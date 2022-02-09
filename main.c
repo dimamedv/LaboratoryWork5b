3bc9f9bdba408d032c2be1dc74b5a6bf3a289e3e
dmedv <dimamedv1@gmail.com>
2022-02-09 18:12:54 +0300

    refactoring / bug fix

--- main.c
+++ main.c
@@ -74,21 +74,7 @@ void test() {
 int main() {
     test();
 
-    size_t n;
-    scanf("%zd", &n);
-
-    vectorVoid v = createVectorV(0, sizeof(float));
-    for (int i = 0; i < n; i++) {
-        float x;
-        scanf("%f", &x);
-        pushBackV(&v, &x);
-    }
-    for (int i = 0; i < n; i++) {
-        float x;
-        getVectorValueV(&v, i, &x);
-        printf("%f ", x);
-    }
-
+    printf("Tests are OK!");
     return 0;
 }
 
